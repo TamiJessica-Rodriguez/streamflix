2789a85f9c5145a2706de3630258404beccbccc9
swenat <lysefeldt@outlook.com>
2024-03-25 20:28:47 +0100

    added background gold gradient

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,9 @@
 function Header() {
 	return (
-		<div className="flex flex-col sm:flex-row items-center bg-white text-black p-4">
+		<div
+			className="flex flex-col sm:flex-row items-center bg-gradient-to-r from-yellow-600 via-yellow-300 to-yellow-600
+        text-white p-4"
+		>
 			<h1>Detta är vår header</h1>
 		</div>
 	);
