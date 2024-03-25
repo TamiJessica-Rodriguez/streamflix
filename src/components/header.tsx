d1a6fd553472c6ee1c4459ef566c3aabbe386d63
swenat <lysefeldt@outlook.com>
2024-03-25 20:27:47 +0100

    added flex flex col and for smaller units with items-centered

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,9 @@
-function Header () {
-    return (
-        <div className="bg-gray-950 text-white p-4">
-            <h1>Detta är vår header</h1>
-        </div>
-    )
+function Header() {
+	return (
+		<div className="flex flex-col sm:flex-row items-center bg-white text-black p-4">
+			<h1>Detta är vår header</h1>
+		</div>
+	);
 }
 
-export default Header
+export default Header;
