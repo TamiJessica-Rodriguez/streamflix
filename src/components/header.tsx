7ea57d9057f56a597742c019ef787e5c9be5a8ae
swenat <lysefeldt@outlook.com>
2024-03-25 20:43:25 +0100

    added logoimage and link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,19 @@
+import { Link } from "react-router-dom";
+
 function Header() {
 	return (
 		<div
 			className="flex flex-col sm:flex-row items-center bg-gradient-to-r from-yellow-600 via-yellow-300 to-yellow-600
         text-white p-4"
 		>
-			<h1>Detta är vår header</h1>
+			<Link to="/">
+				<img
+					src="src\images\Streamflix.png"
+					alt="Logo"
+					className="h-7 w-13 mb-2 sm:mb-0 sm:mr-2"
+				/>
+			</Link>
+			<h1 className="text-lg sm:text-xl">Detta är vår header</h1>{" "}
 		</div>
 	);
 }
