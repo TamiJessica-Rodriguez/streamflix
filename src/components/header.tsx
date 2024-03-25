34e1453e27eb2729cb7f4e43f6764a23443211ee
swenat <lysefeldt@outlook.com>
2024-03-25 21:42:59 +0100

    added more links and styling to them for responsiveness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,14 +6,27 @@ function Header() {
 			className="flex flex-col sm:flex-row items-center bg-gradient-to-r from-yellow-600 via-yellow-300 to-yellow-600
         text-white p-4"
 		>
-			<Link to="/">
-				<img
-					src="src\images\Streamflix.png"
-					alt="Logo"
-					className="h-7 w-13 mb-2 sm:mb-0 sm:mr-2"
-				/>
-			</Link>
-			<h1 className="text-lg sm:text-xl">Detta är vår header</h1>{" "}
+			{" "}
+			<div className="flex justify-start items-center sm:mr-4 p-2 pr-0">
+				<Link to="/">
+					<img
+						src="src\images\Streamflix.png"
+						alt="Logo"
+						className="h-7 w-13 mb-2 sm:mb-0 sm:mr-2"
+					/>
+				</Link>
+			</div>
+			<div className="flex flex-grow justify-center sm:justify-center p-2 pl-0 font-bold font-mono text-lg text-black">
+				<Link to="/" className="p-2 text-black">
+					Home
+				</Link>
+				<Link to="/PreviewPage" className="p-2">
+					Preview
+				</Link>
+				<Link to="/Bookmarked" className="p-2">
+					Favorites
+				</Link>
+			</div>
 		</div>
 	);
 }
