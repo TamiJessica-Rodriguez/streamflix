bd84dc5450422277c6ee1adac657747df28e56b3
Tami Rodriguez <113520005+TamiJessica-Rodriguez@users.noreply.github.com>
2024-03-26 11:33:29 +0100

    Added functionality so that the movie are rendered on Info page when clicking on it in Bookmarked Page.

--- src/pages/Bookmarked.tsx
+++ src/pages/Bookmarked.tsx
@@ -10,30 +10,29 @@ export default function Bookmarked() {
   );
 
   return (
-    // Sätter bakgrundsfärgen till svart för hela komponenten
     <div
       style={{ backgroundColor: "black", minHeight: "100vh", color: "white" }}
     >
       <div className="container mx-auto py-8">
-        <Link to={"/PreviewPage"}>
-          <button
-            className="mb-4"
-            style={{ color: "black", backgroundColor: "white" }}
-          >
+        <Link to="/PreviewPage">
+          <button style={{ color: "black", backgroundColor: "white" }}>
             PreviewPage
           </button>
         </Link>
         <div className="grid grid-cols-4 gap-4">
           {bookmarkedMovies.map((movie) => (
-            <div
-              key={movie.id}
-              className="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white"
-            >
-              <img className="w-full" src={movie.thumbnail} alt={movie.title} />
-              <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{movie.title}</div>
+            <Link to={`/infoPage/${movie.id}`} key={movie.id}>
+              <div className="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white">
+                <img
+                  className="w-full"
+                  src={movie.thumbnail}
+                  alt={movie.title}
+                />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">{movie.title}</div>
+                </div>
               </div>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
