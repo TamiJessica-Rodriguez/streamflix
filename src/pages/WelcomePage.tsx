9e214f3790cce4c2543fc34e056dfed39122cbe6
swenat <lysefeldt@outlook.com>
2024-03-26 20:52:08 +0100

    changed ease in seconds

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -24,7 +24,7 @@ const WelcomePage: React.FC = () => {
                                     font-weight: bold;
                                 }
                                 .welcome-paragraph, .enter-button {
-                                    animation: fadeIn 5s ease-in-out;
+                                    animation: fadeIn 7s ease-in-out;
                                 }
                                 @keyframes fadeIn {
                                     0% {
