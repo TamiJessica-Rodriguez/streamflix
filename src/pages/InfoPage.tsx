59f5d1eba0ef43caa2a2b7d743ce5fa7e9e75a17
Michaela <mi-and@outlook.com>
2024-03-26 16:15:11 +0100

    Added button heart

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -45,6 +45,21 @@ const InfoPage: React.FC<InfoPageProps> = ({ movieId, onCloseModal }) => {
             ref={modalRef}
           >
             <div className="p-4">
+              <button
+                // onClick={() => toggleBookmark(movie.id)}
+                className="absolute top-0 right-0 p-2"
+                aria-label="Bookmark"
+              >
+                <svg
+                  viewBox="0 0 24 24"
+                  // fill={bookmarked.has(movie.id) ? "lightgray" : "none"}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                  strokeWidth="2"
+                >
+                  <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                </svg>
+              </button>
               <div className="text-center"></div>
               <div className="mt-4">
                 <img src={movie?.thumbnail} alt="" className="mx-auto w-52" />
