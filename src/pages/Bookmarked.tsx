289a334a0d4cb8808ff7c04d72fb69a87eb033a5
Tami Rodriguez <113520005+TamiJessica-Rodriguez@users.noreply.github.com>
2024-03-26 11:38:52 +0100

    Cleaned up code

--- src/pages/Bookmarked.tsx
+++ src/pages/Bookmarked.tsx
@@ -1,45 +1,3 @@
-// import { Link } from "react-router-dom";
-// import { mockedData } from "../data/mockedData";
-
-// export default function Bookmarked() {
-//   const bookmarkedIds = JSON.parse(
-//     localStorage.getItem("bookmarkedMovies") || "[]"
-//   );
-//   const bookmarkedMovies = mockedData.filter((movie) =>
-//     bookmarkedIds.includes(movie.id)
-//   );
-
-//   return (
-//     <div
-//       style={{ backgroundColor: "black", minHeight: "100vh", color: "white" }}
-//     >
-//       <div className="container mx-auto py-8">
-//         <Link to="/PreviewPage">
-//           <button style={{ color: "black", backgroundColor: "white" }}>
-//             PreviewPage
-//           </button>
-//         </Link>
-//         <div className="grid grid-cols-4 gap-4">
-//           {bookmarkedMovies.map((movie) => (
-//             <Link to={`/infoPage/${movie.id}`} key={movie.id}>
-//               <div className="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white">
-//                 <img
-//                   className="w-full"
-//                   src={movie.thumbnail}
-//                   alt={movie.title}
-//                 />
-//                 <div className="px-6 py-4">
-//                   <div className="font-bold text-xl mb-2">{movie.title}</div>
-//                 </div>
-//               </div>
-//             </Link>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 import { Link } from "react-router-dom";
 import { mockedData } from "../data/mockedData";
 
@@ -76,7 +34,6 @@ export default function Bookmarked() {
                 />
                 <div className="px-6 py-4">
                   <div className="font-bold text-xl mb-2">{movie.title}</div>
-                  {/* Övrig text här kan också specificeras att vara svart om så önskas */}
                 </div>
               </div>
             </Link>
