a6b5de0236b9f11d6c1413aaa79fbac4d77275b5
swenat <lysefeldt@outlook.com>
2024-03-27 11:30:54 +0100

    margin zero on button and upper div

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -4,16 +4,16 @@ import VideoComponent from "../components/VideoComponent";
 
 const WelcomePage: React.FC = () => {
 	return (
-		<div className="bg-black text-white min-h-screen flex flex-col justify-center items-center">
+		<div className="bg-black text-white min-h-screen flex flex-col justify-center items-center mb-0">
 			<VideoComponent />
-			<p className="text-2xl text-center mb-4 mt-3 welcome-paragraph">
+			<p className="text-2xl text-center mb-3 mt-3 welcome-paragraph">
 				<span style={{ color: "gold", fontWeight: "bold" }}>
 					Welcome to our world of entertainment.
 				</span>
 			</p>
 			<div className="p-2">
 				<Link to={"/PreviewPage"}>
-					<button className="relative inline-flex justify-center items-center w-48 md:w-64 h-12 rounded-full text-black text-xl font-semibold font-sans transition-all duration-300 focus:outline-none enter-button">
+					<button className="relative inline-flex justify-center items-center w-48 md:w-64 h-12 rounded-full text-black text-xl font-semibold font-sans transition-all duration-300 focus:outline-none enter-button mb-0">
 						<span className="text-halo z-10">Enter</span>
 						<span className="absolute inset-0 bg-gradient-to-r from-purple-800 via-purple-500 to-pink-200 rounded-full filter blur-xl transition-all duration-300 z-0"></span>
 						<span className="absolute inset-0 bg-gradient-to-r from-yellow-400 via-yellow-200 to-orange-500 rounded-full transition-all duration-300 z-0"></span>{" "}
