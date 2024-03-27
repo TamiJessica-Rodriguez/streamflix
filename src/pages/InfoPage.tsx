668181b925096fec7dad54dae7897b8e44212f73
Michaela <mi-and@outlook.com>
2024-03-27 12:59:54 +0100

    Fixed movie.id error

--- src/pages/InfoPage.tsx
+++ src/pages/InfoPage.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useRef } from "react";
 import { useBookmarked } from "../context/BookmarkedContext";
-import { mockedData } from "../data/mockedData";
+import { Movie, mockedData } from "../data/mockedData";
 
 interface InfoPageProps {
-  movieId: string; // Lägg till movieId i props
+  movieId: string;
   onCloseModal: () => void;
 }
 
 const InfoPage: React.FC<InfoPageProps> = ({ movieId, onCloseModal }) => {
-  const movie = mockedData.find((movie) => movie.id.toString() === movieId);
+  const movie: Movie | undefined = mockedData.find(
+    (movie) => movie.id.toString() === movieId
+  );
   const modalRef = useRef<HTMLDivElement>(null);
   const { bookmarked, toggleBookmark } = useBookmarked();
 
@@ -46,33 +48,39 @@ const InfoPage: React.FC<InfoPageProps> = ({ movieId, onCloseModal }) => {
             ref={modalRef}
           >
             <div className="p-4">
-              <button
-                onClick={() => toggleBookmark()}
-                className="absolute top-0 right-0 p-2"
-                aria-label="Bookmark"
-              >
-                <svg
-                  viewBox="0 0 24 24"
-                  fill={bookmarked.has() ? "lightgray" : "none"}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                  strokeWidth="2"
+              {movie && (
+                <button
+                  onClick={() => toggleBookmark(movie.id)}
+                  className="absolute top-0 right-0 p-2"
+                  aria-label="Bookmark"
                 >
-                  <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                </svg>
-              </button>
+                  <svg
+                    viewBox="0 0 24 24"
+                    fill={bookmarked.has(movie.id) ? "lightgray" : "none"}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                    strokeWidth="2"
+                  >
+                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                  </svg>
+                </button>
+              )}
               <div className="text-center"></div>
               <div className="mt-4">
-                <img src={movie?.thumbnail} alt="" className="mx-auto w-52" />
-                <h3 className="text-lg font-medium text-white">
-                  {movie?.title}
-                </h3>
-                <p>{movie?.genre}</p>
-                <p>{movie?.year}</p>
+                {movie && (
+                  <img src={movie.thumbnail} alt="" className="mx-auto w-52" />
+                )}
+                {movie && (
+                  <h3 className="text-lg font-medium text-white">
+                    {movie.title}
+                  </h3>
+                )}
+                {movie && <p>{movie.genre}</p>}
+                {movie && <p>{movie.year}</p>}
                 <br />
 
-                <p>{movie?.synopsis}</p>
-                <p>{movie?.actors}</p>
+                {movie && <p>{movie.synopsis}</p>}
+                {movie && <p>{movie.actors}</p>}
               </div>
             </div>
           </div>
