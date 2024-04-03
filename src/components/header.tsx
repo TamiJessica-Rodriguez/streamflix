db7141bd5f53d779958b9dce7362fc11528f5901
Swenat <lysefeldt@outlook.com>
2024-04-03 10:49:03 +0200

    footer eslint fix and header
    Co-authored-by: Navas <Navaas@users.noreply.github.com>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,64 +7,64 @@ import { useRating } from "../context/RatingContext";
 const FEATURE_FLAG_RATING_SYSTEM = false;
 
 function Header() {
-  const { rating, setRating } = useRating();
-  const [ratingVisible, setRatingVisible] = useState(false);
+	const { rating, setRating } = useRating();
+	const [ratingVisible, setRatingVisible] = useState(false);
 
-  const toggleRatingVisibility = () => {
-    if (FEATURE_FLAG_RATING_SYSTEM) {
-      setRatingVisible(!ratingVisible);
-    }
-  };
+	const toggleRatingVisibility = () => {
+		if (FEATURE_FLAG_RATING_SYSTEM) {
+			setRatingVisible(!ratingVisible);
+		}
+	};
 
-  const handleRating = (index: number) => {
-    const newRating = index + 1;
-    setRating(newRating);
-  };
+	const handleRating = (index: number) => {
+		const newRating = index + 1;
+		setRating(newRating);
+	};
 
-  return (
-    <div className="relative flex flex-col flex-1 sm:flex-row items-center justify-between bg-gradient-to-r from-yellow-600 via-yellow-300 to-yellow-600 text-white p-2">
-      <div className="flex flex-1 items-center justify-start sm:mr-4">
-        <Link to="/">
-          <img src="src/images/newlogo.png" alt="Logo" className="h-10" />
-        </Link>
-      </div>
-      <div className="flex-1 flex justify-center items-center font-bold text-black text-xl">
-        <Link to="/" className="px-2 text-black m-2 hover:text-white">
-          Home
-        </Link>
-        <Link to="/PreviewPage" className="px-2 m-2 hover:text-white">
-          Preview
-        </Link>
-      </div>
-      <div className="flex flex-1 justify-end items-center sm:mr-4">
-        <Link to="/Bookmarked" className="text-black hover:text-white mr-4">
-          <FontAwesomeIcon icon={faHeart} />
-        </Link>
-        {FEATURE_FLAG_RATING_SYSTEM && (
-          <button
-            onClick={toggleRatingVisibility}
-            className="text-black hover:text-white"
-          >
-            <FontAwesomeIcon icon={faStar} />
-          </button>
-        )}
-      </div>
-      {ratingVisible && FEATURE_FLAG_RATING_SYSTEM && (
-        <div className="absolute top-full right-0 mt-2 bg-black p-4 shadow-lg rounded z-10 flex justify-around w-48">
-          {Array.from({ length: 5 }, (_, index) => (
-            <FontAwesomeIcon
-              key={index}
-              icon={faStar}
-              onClick={() => handleRating(index)}
-              className={`cursor-pointer ${
-                index < rating ? "text-yellow-500" : "text-gray-400"
-              }`}
-            />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className="relative flex flex-1 flex-col items-center justify-between bg-gradient-to-r from-yellow-600 via-yellow-300 to-yellow-600 p-2 text-white sm:flex-row">
+			<div className="flex flex-1 items-center justify-start sm:mr-4">
+				<Link to="/">
+					<img src="src/images/newlogo.png" alt="Logo" className="h-10" />
+				</Link>
+			</div>
+			<div className="flex flex-1 items-center justify-center text-xl font-bold text-black">
+				<Link to="/" className="m-2 px-2 text-black hover:text-white">
+					Home
+				</Link>
+				<Link to="/PreviewPage" className="m-2 px-2 hover:text-white">
+					Preview
+				</Link>
+			</div>
+			<div className="flex flex-1 items-center justify-end sm:mr-4">
+				<Link to="/Bookmarked" className="mr-4 text-black hover:text-white">
+					<FontAwesomeIcon icon={faHeart} />
+				</Link>
+				{FEATURE_FLAG_RATING_SYSTEM && (
+					<button
+						onClick={toggleRatingVisibility}
+						className="text-black hover:text-white"
+					>
+						<FontAwesomeIcon icon={faStar} />
+					</button>
+				)}
+			</div>
+			{ratingVisible && FEATURE_FLAG_RATING_SYSTEM && (
+				<div className="absolute right-0 top-full z-10 mt-2 flex w-48 justify-around rounded bg-black p-4 shadow-lg">
+					{Array.from({ length: 5 }, (_, index) => (
+						<FontAwesomeIcon
+							key={index}
+							icon={faStar}
+							onClick={() => handleRating(index)}
+							className={`cursor-pointer ${
+								index < rating ? "text-yellow-500" : "text-gray-400"
+							}`}
+						/>
+					))}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default Header;
